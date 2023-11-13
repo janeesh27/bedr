efdc31f090a5a90506a2d7e2ae551df53d408b14
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-11-13 23:13:05 +0530

    ADD : fonts + variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
 
 function App() {
-    return <div> Hello World</div>;
+    return (
+        <div>
+            <h1>TESTING FONTS</h1>
+
+            <h2>Kudi kendi nigga marda</h2>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus explicabo incidunt, at blanditiis ut
+                maiores iste odit repudiandae assumenda asperiores ullam non expedita nulla minus quis nemo itaque quo.
+                Ipsa.0
+            </p>
+        </div>
+    );
 }
 
 export default App;
