0a469b3c6daeab31a339fcf28d9de011ec829c9d
janeesh27 <janeesh.tin@gmail.com>
2023-11-14 19:37:33 +0530

    tailwindcss added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-    return (
-        <div>
-            <h1>TESTING FONTS</h1>
+  return (
+    <div>
+      <h1 className="text-center">TESTING FONTS</h1>
 
-            <h2>Kudi kendi nigga marda</h2>
+      <h2>Kudi kendi nigga marda</h2>
 
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus explicabo incidunt, at blanditiis ut
-                maiores iste odit repudiandae assumenda asperiores ullam non expedita nulla minus quis nemo itaque quo.
-                Ipsa.0
-            </p>
-        </div>
-    );
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
+        explicabo incidunt, at blanditiis ut maiores iste odit repudiandae
+        assumenda asperiores ullam non expedita nulla minus quis nemo itaque
+        quo. Ipsa.0
+      </p>
+    </div>
+  );
 }
 
 export default App;
