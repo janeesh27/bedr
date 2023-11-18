d49fc01f734c4b49a300c0075bdb3b55a34abcfb
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-11-18 19:28:29 +0530

    ADD : Hero component for mobile

--- src/sections/Hero/index.jsx
+++ src/sections/Hero/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import heroImg from '../../assets/hero.svg';
+
+import styles from './index.module.css';
+
 const Hero = () => {
-    return <div>Hero section</div>;
+    return (
+        <main className="bg-primary-blue pt-[94px]">
+            <h1 className="text-4xl text-center	px-10 font-bold text-white leading-12">
+                STEP INTO AN AFFORDABLE HAVEN FOR STUDENT LIVING
+            </h1>
+
+            <section>
+                <p className="text-center px-8 text-white font-light text-sm mt-2 ">
+                    Our platform is dedicated to connecting students with a diverse range of housing options across
+                    India
+                </p>
+            </section>
+
+            <button className="px-4 py-2 rounded bg-secondary-peach font-medium text-secondary-dark-blue text-xs block mx-auto mt-8 cursor-pointer">
+                START RENTING
+            </button>
+
+            <img src={heroImg} alt="hero-img" className={`${styles.heroImg} mx-auto relative top-[-15px]`} />
+        </main>
+    );
 };
 
 export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'primary-blue': 'var(--primary-blue)',
+                'secondary-peach': 'var(--secondary-peach)',
+                'secondary-dark-blue': 'var(--secondary-dark-blue)',
+            },
+        },
+    },
+    plugins: [],
 };
