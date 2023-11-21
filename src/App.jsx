2c158c106ffd6533ce5cafef74d03cf6ebf79b75
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-11-21 20:06:07 +0530

    ADD : GetABed section

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import "./App.css";
-import Hero from "./sections/Hero";
-import Features from "./sections/Features";
-import GetABed from "./sections/GetABed";
-import PgOwner from "./sections/PgOwner";
-import Community from "./sections/Community";
-import ThankyouPage from "./sections/ThankyouPage";
+import './App.css';
+import Hero from './sections/Hero';
+import Features from './sections/Features';
+import GetABed from './sections/GetABed';
+import PgOwner from './sections/PgOwner';
+import Community from './sections/Community';
+import ThankyouPage from './sections/ThankyouPage';
 
-import Navbar from "./components/Navbar";
-import VerticalSpacer from "./components/VerticalSpacer";
+import Navbar from './components/Navbar';
+import VerticalSpacer from './components/VerticalSpacer';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <VerticalSpacer />
-      <Features />
-      <VerticalSpacer />
-      {/* <GetABed /> */}
-      <VerticalSpacer />
-      <PgOwner />
-      <VerticalSpacer />
-      <Community />
-      {/* <ThankyouPage /> */}
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <VerticalSpacer />
+            <Features />
+            <VerticalSpacer />
+            <GetABed />
+            <VerticalSpacer />
+            <PgOwner />
+            <VerticalSpacer />
+            <Community />
+            {/* <ThankyouPage /> */}
+        </>
+    );
 }
 
 export default App;
